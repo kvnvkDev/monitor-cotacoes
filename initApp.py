b6ca2df40aa82b02be413582c6ab441b6abf3d87
Kvn Rc <kvnvk3@gmail.com>
2024-08-26 14:49:42 -0300

    script. arquivo de inicialização com threads para app e web

---
+++ initApp.py
@@ -0,0 +1,14 @@
+from app import *
+from web import *
+
+import threading
+
+
+threadWeb = threading.Thread(target=web.run).start()
+app = App()
+thread = threading.Thread(target=App.ini(App))
+      
+thread.start()
+    
+exit()
+#notificação
